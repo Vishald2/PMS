2317e257fd402b930e66bbf73825e3e328c8b109
vishal <vishald24@yahoo.com>
2020-01-18 23:07:42 +0530

    Add project files.

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long simpleInterest;
+
+            long principal, rate, duration;
+
+            principal = long.Parse(txtPrincipal.Text);
+
+            rate = long.Parse(txtRate.Text);
+
+            duration = long.Parse(txtDuration.Text);
+
+            simpleInterest = principal * rate * duration / 100;
+
+            lblResult.Text = simpleInterest.ToString();
+
+        }
+    }
+}
